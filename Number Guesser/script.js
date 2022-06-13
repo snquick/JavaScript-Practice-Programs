d81b9fba8abf8c8bb72ab692e7f7f5be76bf0c22
snquick <stephanienquick@lewisu.edu>
2022-06-13 16:19:16 -0500

    finished number guesser

--- Number Guesser/script.js
+++ Number Guesser/script.js
@@ -1,8 +1,8 @@
 const num = Math.ceil(Math.random() * 10);
 console.log(num);
- const gnum = prompt('Guess the number between 1 and 10 inclusive');
+ const gnum = prompt('Guess a number between 1 and 10!');
  if (gnum == num)
    console.log('Matched');
   else
-   console.log('Not matched, the number was '+gnum);
+   console.log('Oops, not match a match. The number was '+gnum);
    
