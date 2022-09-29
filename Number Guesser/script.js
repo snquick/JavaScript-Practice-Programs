3c4ef1e2855d567ab1c97459a6593802fe3f66fc
Stephanie Quick <35506354+snquick@users.noreply.github.com>
2022-09-29 12:22:15 -0500

    Delete Number Guesser directory

--- Number Guesser/script.js
+++
@@ -1,8 +0,0 @@
-const num = Math.ceil(Math.random() * 10);
-console.log(num);
- const gnum = prompt('Guess a number between 1 and 10!');
- if (gnum == num)
-   console.log('Matched');
-  else
-   console.log('Oops, not match a match. The number was '+gnum);
-   
